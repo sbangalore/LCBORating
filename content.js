781a0170610e67f3da6e6b5bada6bbbd56d2a150
Sudarshan Bangalore <bsudarshan@outlook.com>
2019-07-16 22:58:00 -0400

    fetch names done

--- content.js
+++ content.js
@@ -1,1 +1,11 @@
-console.log("Chrome extension go")
+console.log("Chrome extension");
+let paragraphs = document.getElementsByClassName('product_name');
+var i = 1;
+var re = />(.*)</g;
+for (elt of paragraphs) {
+	console.log('product #' + i);
+	var res = re.exec(elt.innerHTML)[1];
+	console.log(res);
+	i++;
+	break;
+}
